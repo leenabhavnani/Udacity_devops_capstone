bb4c0910da348fcca677a6a0147e5d9197e19b6b
Leena Bhavnani <leenabhavnani@yahoo.co.in>
2021-07-27 23:36:01 +0530

    added project code

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-const path = require('path');
-const fs = require('fs');
-
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 
-const filePath = path.join(__dirname, 'story', 'text.txt');
-
-app.use(bodyParser.json());
-
-app.get('/story', (req, res) => {
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      return res.status(500).json({ message: 'Failed to open file.' });
-    }
-    res.status(200).json({ story: data.toString() });
-  });
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!</h1>
+    <p>Try sending a request to /error and see what happens</p>
+  `);
 });
 
-app.post('/story', (req, res) => {
-  const newText = req.body.text;
-  if (newText.trim().length === 0) {
-    return res.status(422).json({ message: 'Text must not be empty!' });
-  }
-  fs.appendFile(filePath, newText + '\n', (err) => {
-    if (err) {
-      return res.status(500).json({ message: 'Storing the text failed.' });
-    }
-    res.status(201).json({ message: 'Text was stored!' });
-  });
-});
-
-app.get('/error', () => {
+app.get('/error', (req, res) => {
   process.exit(1);
 });
 
-app.listen(3000);
+app.listen(8080);
