eed4d1c1b7e70c64b3d0b857bc2ad5e8226b1111
Leena Bhavnani <leenabhavnani@yahoo.co.in>
2021-07-31 15:22:32 +0530

    smoke test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
-	<h2 style="color:blue">This is the Blue deployment </h2>
+	<h2 style="color:green">This is the Green deployment </h2>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
