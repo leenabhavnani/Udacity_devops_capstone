5c3b420ac26e841b54098c5fe12702ad6ea4e7ed
Leena Bhavnani <leenabhavnani@yahoo.co.in>
2021-07-31 14:07:43 +0530

    deployment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
+	<h2 style="color:blue">This is the Blue deployment </h2>
     <p>Try sending a request to /error and see what happens</p>
   `);
 });
